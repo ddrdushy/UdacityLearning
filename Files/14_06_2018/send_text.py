8fc3517a8678d48e0193dec632872771999b4f2e
ddrdushy <dushy2009@gmail.com>
2018-06-14 22:36:46 +0800

    created the project to send text message using twilo

---
+++ Files/14_06_2018/send_text.py
@@ -0,0 +1,15 @@
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "<SID>"
+# Your Auth Token from twilio.com/console
+auth_token  = "<TOKEN>"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="<RECEIPENT>", 
+    from_="<TWILLO_NUMBER>",
+    body="Hello from Python!")
+
+print(message.sid)
